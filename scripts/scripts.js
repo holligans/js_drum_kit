9574bc75907f70777616de254719a14e58bf1f48
holligans1985 <alexfranc1985@gmail.com>
2018-02-12 23:20:23 -0500

    drum working fine

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,24 @@
+playSound = event => {
+  // get the key code number
+  const key = event.keyCode;
+  const sound = document.querySelector(`audio[data-key="${key}"]`);
+  if (!sound) return;
+  // play the audio with the same code number
+  sound.currentTime = 0;
+  sound.play();
+  // add effect when click with the css class "playing" to the div with the same key number
+  const instrument = document.querySelector(`div[data-key="${key}"]`);
+  instrument.classList.add("playing");
+};
+
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
 // detect keydown event
+window.addEventListener("keydown", playSound);
 
-// get the key code number
-
-// play the audio with the same code number
-
-// add animation to the div with the same key number
+// Add eventListeners to remove transition
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
